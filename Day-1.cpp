63b68d03b6435a2c1394666eddd1c2fd644159a0
tom380 <62703522+tom380@users.noreply.github.com>
2022-12-01 12:42:40 +0100

    Added part 1

--- Day-1.cpp
+++ Day-1.cpp
@@ -1,7 +1,30 @@
-#include <vector>
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int main() {
-
-  return 0;
+int main()
+{
+    std::string line;
+    std::ifstream MyReadFile("input.txt");
+    
+    int maxCal = 0;
+    int curCal = 0;
+    
+    while (getline (MyReadFile, line)) {
+        if (std::atoi(line.c_str()) != 0) {
+            curCal += std::atoi(line.c_str());
+            //std::cout << std::atoi(line.c_str()) << std::endl;
+        }
+        else {
+            if (curCal > maxCal) maxCal = curCal;
+            curCal = 0;
+            //std::cout << "newline" << std::endl;
+        }
+    }
+    
+    std::cout << maxCal << std::endl;
+    
+    // Close the file
+    MyReadFile.close();
+    return 0;
 }
