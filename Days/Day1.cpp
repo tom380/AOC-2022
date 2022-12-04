1fc83399943f672b025a52377e1e14b3ee192f14
tom380 <twjfransen@gmail.com>
2022-12-04 22:40:00 +0100

    Fixed day 1 part 1

--- Days/Day1.cpp
+++ Days/Day1.cpp
@@ -5,15 +5,15 @@ int Day1::part1(std::string input)
     std::string line;
     std::ifstream file(input);
 
-    int maxCal = 2;
+    int maxCal = 0;
     int curCal = 0;
     
     while (getline (file, line)) {
         if (line != "") {
             curCal += std::stoi(line);
         }
-        else if (curCal > maxCal) {
-            maxCal = curCal;
+        else {
+            if (curCal > maxCal) maxCal = curCal;
             curCal = 0;
         }
     }
