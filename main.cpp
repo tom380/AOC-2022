bee1cd6363a3134b44f9c311ea34e883a8f26a7f
tom380 <twjfransen@gmail.com>
2022-12-05 19:26:09 +0100

    Changed output style

--- main.cpp
+++ main.cpp
@@ -6,17 +6,17 @@ int main() {
 	std::cout << std::ifstream("title.txt").rdbuf() << std::endl << std::endl;
 
 	std::cout <<
-		"Day 1: Calorie Counting" << std::endl <<
+		"--- Day 1: Calorie Counting ---" << std::endl <<
 		"\tPart 1: " << Day1::part1("Inputs/day1.txt") << std::endl <<
-		"\tPart 2: " << Day1::part2("Inputs/day1.txt") << std::endl <<
+		"\tPart 2: " << Day1::part2("Inputs/day1.txt") << std::endl << std::endl <<
 
-		"Day 2: Rock Paper Scissors" << std::endl <<
+		"--- Day 2: Rock Paper Scissors ---" << std::endl <<
 		"\tPart 1: " << Day2::part1("Inputs/day2.txt") << std::endl <<
-		"\tPart 2: " << Day2::part2("Inputs/day2.txt") << std::endl <<
+		"\tPart 2: " << Day2::part2("Inputs/day2.txt") << std::endl << std::endl <<
 
-		"Day 3: Rucksack Reorganization" << std::endl <<
+		"--- Day 3: Rucksack Reorganization ---" << std::endl <<
 		"\tPart 1: " << Day3::part1("Inputs/day3.txt") << std::endl <<
-		"\tPart 2: " << Day3::part2("Inputs/day3.txt") << std::endl;
+		"\tPart 2: " << Day3::part2("Inputs/day3.txt") << std::endl << std::endl;
 
 	return 0;
 }
