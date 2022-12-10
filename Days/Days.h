92f38f5c3169b13f643380307422904a3715cd20
tom380 <twjfransen@gmail.com>
2022-12-10 14:34:57 +0100

    Changed to namespace structure

--- Days/Days.h
+++ Days/Days.h
@@ -8,52 +8,42 @@
 #include <algorithm>
 #include <numeric>
 
-class Day1 {
-public:
-	static int part1(std::string input);
-	static int part2(std::string input);
-};
+namespace Day1 {
+	int part1(std::string input);
+	int part2(std::string input);
+}
 
-class Day2 {
-public:
-	static int part1(std::string input);
-	static int part2(std::string input);
-};
+namespace Day2 {
+	int part1(std::string input);
+	int part2(std::string input);
+}
 
-class Day3 {
-public:
-	static int part1(std::string input);
-	static int part2(std::string input);
-private:
-	static char findCommonChar(std::vector<std::string> strings);
-};
+namespace Day3 {
+	int part1(std::string input);
+	int part2(std::string input);
+	char findCommonChar(std::vector<std::string> strings);
+}
 
-class Day4 {
-public:
-	static int part1(std::string input);
-	static int part2(std::string input);
-};
+namespace Day4 {
+	int part1(std::string input);
+	int part2(std::string input);
+}
 
-class Day5 {
-public:
-	static std::string part1(std::string input);
-	static std::string part2(std::string input);
-};
+namespace Day5 {
+	std::string part1(std::string input);
+	std::string part2(std::string input);
+}
 
-class Day6 {
-public:
-	static int part1(std::string input);
-	static int part2(std::string input);
-private:
-	static bool hasDuplicate(std::vector<char> v);
-};
+namespace Day6 {
+	int part1(std::string input);
+	int part2(std::string input);
+	bool hasDuplicate(std::vector<char> v);
+}
 
-class Day7 {
-public:
-	static int part1(std::string input);
-	static int part2(std::string input);
-private:
-	static int scanDir(std::ifstream& commandPrompt, std::vector<int>& dirSizes);
-};
+namespace Day7 {
+	int part1(std::string input);
+	int part2(std::string input);
+	int scanDir(std::ifstream& commandPrompt, std::vector<int>& dirSizes);
+}
 
 #endif
